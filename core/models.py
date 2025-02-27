78d2f6b4505fc43269d395de072ed86a5e525abf
Dimitri <dimitri3329@gmail.com>
2025-02-27 18:25:56 +0400

    add

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    category = models.CharField(max_length=100)
+    description = models.TextField()
+    publication_date = models.DateField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    cover_image = models.ImageField(upload_to='book_covers/')
+
+    def __str__(self):
+        return self.title  # Or something like f"{self.title} by {self.author}"
+
+    class Meta:
+        db_table = 'book'
